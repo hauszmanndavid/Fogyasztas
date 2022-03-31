41b404c06f8e321791a6133c9e88dddb0da98458
hauszmanndavid <93594997+hauszmanndavid@users.noreply.github.com>
2022-03-31 14:25:25 +0200

    script

---
+++ script.js
@@ -0,0 +1,14 @@
+function szamol () {
+    let F = document.getElementById('fogyasztás').value;
+let V = document.getElementById('tank').value;
+let Ut = document.getElementById('ut').value;
+
+let megtehetőUt = V * 100 / F;
+
+
+if (megtehetőUt < Ut ) {
+    document.getElementById('megoldás').innerHTML ="Az út során tankolni kell!" 
+} else{
+    document.getElementById('megoldás').innerHTML = "Az út megtehető tankolás nélkül."};
+}
+
